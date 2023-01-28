ba6ba52c6458f3a55fecaa503db1b1f0390b7a70
JPaul23 <73589069+JPaul23@users.noreply.github.com>
2023-01-28 10:23:10 +0200

    Installing React native element UI

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,10 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import { ButtonPress } from '../components';
 
 const WelcomeScreen = () => {
     return (
         <View>
             <Text>WelcomeScreen</Text>
+            <ButtonPress />
         </View>
     )
 }
